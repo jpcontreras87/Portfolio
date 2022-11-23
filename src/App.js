7853a5aa9075238a8eec298ae77768c3c98837d6
jpcontreras87 <jpcontreras87@gmail.com>
2022-11-23 00:34:31 -0800

    firstdraft

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from "react";
 
 //components
 import Navbar from "./pages/Navbar";
+import ContactMe from "./pages/ContactMe";
 
 //images
-import logo from "./assets/images/myphoto.jpeg";
+import logo from "./assets/images/myphoto2.jpg";
 import linkedin from "./assets/images/linkedin.jpg";
 import github from "./assets/images/github.jpg";
-import twitter from "./assets/images/twitter.jpg";
 
 function App() {
    return (
@@ -24,23 +24,17 @@ function App() {
             <p>Juan P Contreras</p>
             <p class="u-align-justify u-text u-text-grey-40 u-text-3">
                {" "}
-               Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not.
-               It’s not a story the Jedi would tell you. It’s a Sith legend. Darth
-               Plagueis was a Dark Lord of the Sith, so powerful and so wise he could
-               use the Force to influence the midichlorians to create life… He had such
-               a knowledge of the dark side that he could even keep the ones he cared
-               about from dying. The dark side of the Force is a pathway to many
-               abilities some consider to be unnatural. He became so powerful… the only
-               thing he was afraid of was losing his power, which eventually, of
-               course, he did. Unfortunately, he taught his apprentice everything he
-               knew, then his apprentice killed him in his sleep. Ironic. He could save
-               others from death, but not himself
+               I'm a driven individual that highly values knowledge and hard work. I am
+               motivated by challenges in my tasks, I love critical thinking, my work
+               environment and most importantly my family. I aspire to grow in a
+               company with upward mobility that promotes self growth and team
+               building.
             </p>
             <div className="u-social-icons u-spacing-31 u-social-icons-1">
                <img src={linkedin} alt="linkedin" />
                <img src={github} alt="github" />
-               <img src={twitter} alt="twitter" />
             </div>
+            <ContactMe />
          </body>
       </div>
    );
